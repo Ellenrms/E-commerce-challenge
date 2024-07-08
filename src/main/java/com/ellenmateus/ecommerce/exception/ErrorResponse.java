785ca8b2b32fcd903f353be90ed475fd1428eedf
Ellen Mateus <ellenrms@gmail.com>
2024-07-08 11:59:19 -0300

    dependency corrections exception class implementation, swagger implementation

---
+++ src/main/java/com/ellenmateus/ecommerce/exception/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.ellenmateus.ecommerce.exception;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private int code;
+    private String status;
+    private String message;
+}
---
+++ src/main/java/com/ellenmateus/ecommerce/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.ellenmateus.ecommerce.exception;
+
+
+import lombok.Getter;
+
+@Getter
+public class ResourceNotFoundException extends RuntimeException {
+    private final String message;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+        this.message = message;
+    }
+}
